e89aa10c702068e1a8c1788723e39d03fc9c345c
Máté Homolya <mate.homolya@gmail.com>
2020-03-19 09:47:47 +0100

    Now deployment test 12

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 var express = require("express");
 var app = express();
 var http = require("http").Server(app);
-var io = require("socket.io")(http);
+var io = require("socket.io")(http, { serveClient: false });
 var Matter = require("matter-js");
 var extend = require("extend");
 var settings = require("./settings");
